616679045417b639d62a40f98290af20e052bed8
benedito <benethowen@yahoo.com.br>
2022-08-14 21:34:58 -0300

    Feito o carregamento dos dados para a pagina de edição de notícias e podcasts

--- resources/views/editar.blade.php
+++ resources/views/editar.blade.php
@@ -7,46 +7,52 @@
 <section>
 <h2>{{ $titulo }}</h2>
 <img src="/image/desktop-cadastro.jpeg" class="img-fluid mb-5" alt="...">
-@if('tela'== 'noticia')
 
+@foreach($noticias as $noticia )
 <!-- Formulário de notícias -->
 <h2 class="text-center m-5">Postagem de notícias</h2>
-<form action="/posts" method="POST" enctype="multipart/form-data">
+<form action="/edit_not" method="POST" enctype="multipart/form-data">
     @csrf 
     <div class="form-group " id="noticia">
+     
+        <input type="hidden" class="form-control" id="id" name="id" value="{{ $noticia->id }}">
+
+    </div>
+    <div class="form-group " id="noticia">
         <label for="title">Título:</label>
-        <input type="text" class="form-control" id="title" name="title" placeholder="Titulo da notícia">
+        <input type="text" class="form-control" id="title" name="title" value="{{ $noticia->title }}">
 
     </div>
     <div class="form-group">
         <label for="subtitle">Subtítulo:</label>
-        <input type="text" class="form-control" id="subtitle" name="subtitle" placeholder="Subtítulo da notícia">
+        <input type="text" class="form-control" id="subtitle" name="subtitle"value="{{ $noticia->subtitle }}">
 
     </div>
     <div class="form-group">
         <label for="body">Texto:</label>
-        <textarea type="text" class="form-control corpo" id="body" name="body" placeholder="Corpo da notícia"></textarea>
+        <textarea type="text" class="form-control corpo" id="body" name="body" value="{{ $noticia->body }}">{{ $noticia->body }}</textarea>
         
     </div>
     <div class="form-group">
         <label for="image">Fotorreportagem:</label>
-        <input type="file" class="form-control" id="image" name="image" >
+        <input type="file" class="form-control" id="image" name="image" value="storage/{{ $noticia->image}}">
         
     </div>
     <div class="form-group">
         <label for="foto_desc">Legenda da foto:</label>
-        <input type="text" class="form-control" id="foto_desc" name="foto_desc" placeholder="Legenda  da reportagem">
+        <input type="text" class="form-control" id="foto_desc" name="foto_desc" value="{{ $noticia->foto_desc }}">
         
     </div>
     <div class="form-group">
         <label for="data_post">Data do postagem:</label>
-        <input type="text" class="form-control" id="data_post" name="data_post" placeholder="Data da Postagem">
+        <input type="text" class="form-control" id="data_post" name="data_post" value="{{ $noticia->data_post }}">
         
     </div>
-    <input type="submit" class="btn btn-primary" value="Criar post">
+    <br>
+    <input type="submit" class="btn btn-primary" value="Editar">
 
 </form>
-@endif
+@endforeach
 <!-- Formulário de artigos -->
 <h2 class="text-center m-5">Postagem de artigos</h2>
 
@@ -83,11 +89,12 @@
         <input type="text" class="form-control" id="data_post" name="data_post" value="{{ $artigo->data_post }}">
         
     </div>
+    <br>
     <input type="submit" class="btn btn-primary" value="Editar">
 
 </form>
 @endforeach
-<!-- Formulário de fotos -->
+<!-- Formulário de fotos 
 
 <h2 class="text-center m-5">Postagem de fotos</h2>
 
@@ -113,25 +120,26 @@
         <input type="text" class="form-control" id="data_post" name="data_post" placeholder="Data da Postagem">
         
     </div>
+    <br>
     <input type="submit" class="btn btn-primary" value="Salvar foto">
 
 </form>
-
+-->
 
 <!-- Formulário de Podcasts -->
 
 <h2 class="text-center m-5">Postagem de podcasts</h2>
-
-<form action="/posts_podcasts" method="POST" enctype="multipart/form-data">
+@foreach($podcasts as $podc )
+<form action="/edit_podc" method="POST" enctype="multipart/form-data">
     @csrf 
     <div class="form-group">
         <label for="title">Título:</label>
-        <input type="text" class="form-control" id="title" name="title" placeholder="titulo da foto">
+        <input type="text" class="form-control" id="title" name="title" value="{{ $podc->title }}">
 
     </div>
     <div class="form-group">
         <label for="autor">Autor:</label>
-        <input type="text" class="form-control" id="autor" name="autor" placeholder="Nome do autor" >
+        <input type="text" class="form-control" id="autor" name="autor" value="{{ $podc->autor }}" >
 
     </div>
     <div class="form-group">
@@ -141,17 +149,19 @@
     </div>
     <div class="form-group">
         <label for="description">Descrição:</label>
-        <textarea type="text" class="form-control" id="description" name="description" placeholder="Breve descrição"></textarea>
+        <textarea type="text" class="form-control" id="description" name="description" value="{{ $podc->description }}">{{ $podc->description }}</textarea>
         
     </div>
     <div class="form-group">
         <label for="data_post">Data da postagem:</label>
-        <input type="text" class="form-control" id="data_post" name="data_post" placeholder="Data da Postagem">
+        <input type="text" class="form-control" id="data_post" name="data_post" value="{{ $podc->data_post }}">
         
     </div>
-    <input type="submit" class="btn btn-primary" value="Salvar foto">
+    <br>
+    <input type="submit" class="btn btn-primary" value="Editar">
 
 </form>
+@endforeach
 </section>
 
 <script>
